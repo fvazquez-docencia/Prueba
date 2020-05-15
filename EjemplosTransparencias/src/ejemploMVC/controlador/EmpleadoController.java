79b5aa88a9ca52822c45e52fad7e42bcd672399f
fvazquez_docencia <fvazquez.docencia@gmail.com>
2020-05-15 11:28:26 +0200

    .

---
+++ EjemplosTransparencias/src/ejemploMVC/controlador/EmpleadoController.java
@@ -0,0 +1,5 @@
+package ejemploMVC.controlador;
+
+public class EmpleadoController {
+
+}
---
+++ EjemplosTransparencias/src/ejemploMVC/modelo/Empleado.java
@@ -0,0 +1,32 @@
+package ejemploMVC.modelo;
+
+public class Empleado {
+	private String nombre;
+	private String apellidos;
+	private Direccion direccion;
+	
+	public Empleado(String nombre, String apellidos, Direccion direccion)
+	{
+		this.nombre=nombre;
+		this.apellidos=apellidos;
+		this.direccion=direccion;
+	}//Empleado
+	
+	public String getNombre() {
+		return nombre;
+	}//getNombre
+	
+	public String getApellidos() {
+		return apellidos;
+	}//getApellidos
+	
+	public Direccion getDireccion() {
+		return direccion;
+	}//getDireccion
+	
+	@Override
+	public String toString() {
+		return String.format("Nombre:%s\nApellidos:%s\nDireccion:%s",this.nombre,this.apellidos,this.direccion);
+	}//toString
+}//class
+
