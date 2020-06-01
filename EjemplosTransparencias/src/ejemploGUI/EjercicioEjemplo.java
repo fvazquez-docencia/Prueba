9cd189c5965672e7675b28ea8a8888440f643bcd
fvazquez_docencia <fvazquez.docencia@gmail.com>
2020-06-01 19:29:13 +0200

    ..

--- EjemplosTransparencias/src/ejemploGUI/EjercicioEjemplo.java
+++ EjemplosTransparencias/src/ejemploGUI/EjercicioEjemplo.java
@@ -6,8 +6,6 @@ import ejemploGUI.controlador.ControladorPersona;
 import ejemploGUI.vista.EditorPersona;
 
 public class EjercicioEjemplo {
-
-
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
